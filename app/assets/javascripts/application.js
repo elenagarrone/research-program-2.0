94ab148dc7388d38156bd9f3e37f24db07975e7a
elena <elena.15@hotmail.it>
2016-02-14 16:22:45 +0000

    adds autoprefixer and flex display in footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require('browserslist');
+//= require browserslist
 //= require core
 //= require options
 //= require navigation
---
+++ app/assets/javascripts/browserslist.js
@@ -0,0 +1,5 @@
+var browserslist;
+var process = function (css, opts) {
+    var browsers = browserslist(opts.browsers, { path: opts.file });
+    ['> 1%', 'last 2 version']
+}
