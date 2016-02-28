e9bf03bcb71da3a5017fbfd481686e12475d7243
elenagarrone <8742NgM4>
2016-02-28 19:47:26 +0000

    adds fnctionality to add news and edit them

--- app/controllers/news_and_updates_controller.rb
+++ app/controllers/news_and_updates_controller.rb
@@ -1,2 +1,35 @@
 class NewsAndUpdatesController < ApplicationController
+    def index
+      @news = News.all
+    end
+
+    def new
+        @news = News.new
+    end
+
+    def create
+        News.create(news_params)
+        redirect_to '/news'
+    end
+
+    def edit
+        @news = News.find(params[:id])
+    end
+
+    def update
+        @news = News.find(params[:id])
+        @news.update(news_params)
+        redirect_to '/news'
+    end
+
+    def destroy
+        @news = News.find(params[:id])
+        @news.destroy
+        flash[:notice] = 'News deleted successfully'
+        redirect_to '/news'
+    end
+
+    def news_params
+        params.require(:news).permit(:description)
+    end
 end
