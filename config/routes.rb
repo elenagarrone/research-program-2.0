3b4f51a13e4d952556e95469ff5b7cd836c454cb
elenagarrone <8742NgM4>
2016-03-09 22:25:28 +0000

    updates href in links module

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # config/routes.rb
+  get '/:locale' => 'homepage#index'
   root 'homepage#index'
 
   # Example of regular route:
