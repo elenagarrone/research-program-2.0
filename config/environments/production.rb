b1e9991502bfa345ab3640821abfca81d50fa518
elenagarrone <8742NgM4>
2016-03-12 20:01:49 +0000

    adds settings for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
   #Devise mailer settings:
   config.action_mailer.default_url_options = { host: 'https://research-program-2.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
