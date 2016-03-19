38513c329f235c5e4fce7a9f72fca9e0ab88ec14
elenagarrone <8742NgM4>
2016-03-19 11:34:18 +0000

    adds attributes to news

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,9 @@
 class News < ActiveRecord::Base
     validates_presence_of :description
+    attribute :description_it
+    attribute :description_de
+    attribute :description_da
+    attribute :description_es
+    attribute :description_el
+    attribute :description_hu
 end
