2cf6775212caf3a55dad35f09cdf22356155f7e1
elenagarrone <8742NgM4>
2016-03-19 11:40:15 +0000

    updates attr for news

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,9 +1,9 @@
 class News < ActiveRecord::Base
     validates_presence_of :description
-    attribute :description_it
-    attribute :description_de
-    attribute :description_da
-    attribute :description_es
-    attribute :description_el
-    attribute :description_hu
+    attribute :description_it, :validate => false
+    attribute :description_de, :validate => false
+    attribute :description_da, :validate => false
+    attribute :description_es, :validate => false
+    attribute :description_el, :validate => false
+    attribute :description_hu, :validate => false
 end
